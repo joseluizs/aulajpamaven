7905e5748eb94e43b16db141f31e93f21fe42ba5
joseluizs <joseluiz.smarques@gmail.com>
2022-10-11 07:52:26 -0300

    Remove

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -14,8 +14,12 @@ public class Program {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
+		
 		Pessoa p = em.find(Pessoa.class, 2);
-	
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
 		System.out.println(p);
 		System.out.println("Pronto! ");
 		em.close();
